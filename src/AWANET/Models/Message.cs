c3fb85860b5c091015c464a651ed1f9179281388
Sebastian Udden <sebastian.udden@gmail.com>
2016-04-02 20:01:28 +0200

    MessageModel

---
+++ src/AWANET/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWANET.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public string MessageBody { get; set; }
+        public string ImageLink { get; set; }
+        public bool OnFirstPage { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+    }
+}
